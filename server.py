cedcd25816e916516910c408e272a2e9cb6fda04
Mauaqp <peraltilla93@hotmail.com>
2022-01-20 22:58:53 -0500

    Entrega final

--- server.py
+++ server.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 #Ruta
 @app.route( '/', methods=['GET'] )
 def paginaInicial():
-    return render_template("index.html")
+    users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+    return render_template("index.html", usuarios = users)
 
 #App.run
 if __name__ == "__main__":
